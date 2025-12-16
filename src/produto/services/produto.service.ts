56b75d7f69146b2a2ec943810d3e178af654486c
Bobeats . <gabriel.gomes86@gmail.com>
2025-12-16 12:21:29 -0300

    Adicionado o relacionamento entre classes

--- src/produto/services/produto.service.ts
+++ src/produto/services/produto.service.ts
@@ -2,16 +2,22 @@ import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Produto } from "../entities/produto.entity";
 import { DeleteResult, ILike, Repository } from "typeorm";
+import { CategoriaService } from "../../categoria/services/categoria.service";
 
 @Injectable()
 export class ProdutoService {
     constructor(
         @InjectRepository(Produto)
-        private produtoRepository: Repository<Produto>
+        private produtoRepository: Repository<Produto>,
+        private categoriaService: CategoriaService
     ) { }
 
     async findAll(): Promise <Produto[]> {
-        return await this.produtoRepository.find();
+        return await this.produtoRepository.find({
+            relations: {
+                categoria: true
+            }
+        });
     }
 
     async findById(id: number): Promise <Produto> {
@@ -19,6 +25,9 @@ export class ProdutoService {
         const produto= await this.produtoRepository.findOne({
             where: {
                 id
+            }, 
+            relations: {
+                categoria: true
             }
         });
 
@@ -32,17 +41,25 @@ export class ProdutoService {
         return await this.produtoRepository.find({
             where: {
                 titulo: ILike(`%${titulo}%`)
+            },
+            relations: {
+                categoria: true 
             }
         })
     }
 
     async create (produto: Produto): Promise <Produto> {
+
+        await this.categoriaService.findById(produto.categoria.id)
+
         return await this.produtoRepository.save (produto);
     }
 
     async update (produto: Produto): Promise <Produto> {
 
         await this.findById(produto.id)
+
+        await this.categoriaService.findById(produto.categoria.id)
 
         return await this.produtoRepository.save(produto);
     }
