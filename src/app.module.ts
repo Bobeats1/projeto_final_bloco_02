ed816416caef3d8f37f8dfd9a1ec5c13be80f42e
Bobeats . <gabriel.gomes86@gmail.com>
2025-12-16 11:13:36 -0300

    Criada a classe Categoria, e métodos CRUD adicionados

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { CategoriaModule } from './categoria/categoria.module';
+import { Categoria } from './categoria/entities/categoria.entity';
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -13,9 +15,11 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [],
+      entities: [Categoria],
       synchronize: true,
-    })
+      logging: true,
+    }),
+    CategoriaModule,
   ],
   controllers: [],
   providers: [],
